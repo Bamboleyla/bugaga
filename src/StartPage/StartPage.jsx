b28d674c890c11447fa2b0797b2837391de33f59
PC <dvorobjevredstar@mail.ru>
2021-11-04 23:01:18 +0500

    in StartPage i changed RadioButton to a class component

--- src/StartPage/StartPage.jsx
+++ src/StartPage/StartPage.jsx
@@ -1,68 +1,67 @@
-import React, { useEffect } from "react";
+import React from "react";
 import logo from "./logo/logo.svg";
 import style from "./StartPage.module.css";
 import Cell from "./Cell";
 import { NavLink } from "react-router-dom";
+import { Component } from "react";
 
-export const change = (arr) => {
-  let i = -1;
-  return setInterval(() => {
-    document.querySelector("i").className =
-      arr[(i = ++i == arr.length ? 0 : i)];
-  }, 1000);
-};
-
-const StartPage = () => {
-  const arrD = [style.indicator, style.indicatorleft];
-  useEffect(() => {
-    const timerID = change(arrD);
-    return () => clearInterval(timerID);
-  }, []);
-
-  return (
-    <div className={style.grid}>
-      <img src={logo} alt="bugaga"></img>
-
-      <div className={style.radiobutton}>
-        <NavLink to="/radio">RadioButton</NavLink>
-        <div className={style.list}>
-          <div className={style.cell}>
-            <div className={style.togle}>
-              <i className={style.indicatorleft}></i>
+class StartPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dinamic: undefined,
+      i: 0,
+    };
+    let change = (arr) => {
+      let i = this.state.i;
+      return setInterval(() => {
+        let s = arr[(i = ++i == arr.length ? 0 : i)];
+        this.setState({ dinamic: s });
+        return s;
+      }, 1000);
+    };
+    const arrD = [style.indicator, style.indicatorleft];
+    let dinamic = change(arrD);
+  }
+  render() {
+    return (
+      <div className={style.grid}>
+        <img src={logo} alt="bugaga"></img>
+        <div className={style.radiobutton}>
+          <NavLink to="/radio">RadioButton</NavLink>
+          <div className={style.list}>
+            <div className={style.cell}>
+              <div className={style.togle}>
+                <i className={this.state.dinamic}></i>
+              </div>
             </div>
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
           </div>
-          <Cell />
-          <Cell />
-          <Cell />
-          <Cell />
-          <Cell />
-          <Cell />
-          <Cell />
-          <Cell />
+        </div>
+        <div className={style.radio}>
+          <NavLink to="/button">Button</NavLink>
+          <div className={style.list}>
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+          </div>
         </div>
       </div>
-      <div className={style.radio}>
-        <NavLink to="/button">Button</NavLink>
-        <div className={style.list}>
-          <div className={style.cell}>
-            <div className={style.wrap}>
-              <button className={style.buttonOn}>
-                <span className={style.spanOn}>ON</span>
-              </button>
-            </div>
-          </div>
-          <Cell />
-          <Cell />
-          <Cell />
-          <Cell />
-          <Cell />
-          <Cell />
-          <Cell />
-          <Cell />
-        </div>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default StartPage;
